dcc95e2e8c63f1c50ad3a2c946e834c5d0385f4f
Robin Stevens <robin.stevens@scz.be>
2019-12-26 10:47:47 +0100

    Switched to materialize ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,90 @@
 import React from 'react';
 import './App.css';
-import {Rit} from './Rit';
-import {RitOverview} from './RitOverview';
-import {Container, Tabs, Tab} from 'react-bootstrap';
+import { Rit } from './Rit';
+import * as RitOverview from './RitOverview';
+import { Container } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-interface Props{
+interface Props {
 
 }
-interface State{
-  ritten: Rit[];
+interface State {
+  ritten: Rit[],
+  selectedTab: number
 }
 
-export default class App extends React.Component<Props,State>{
-  constructor(props: Props){
+export default class App extends React.Component<Props, State>{
+
+  private handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    this.setState({
+      selectedTab: newValue
+    });
+  };
+
+  constructor(props: Props) {
     super(props);
-
     this.state = {
-      ritten: []
+      ritten: [],
+      selectedTab: 0
     };
-    fetch('resources/ritten.json')
-    .then(response => response.json())
-    .then(data => {
-      this.setState({
-        ritten: data as Rit[]
-      })
-    })
   }
 
-  render(){
+  async componentDidMount() {
+    const resp = await fetch('resources/ritten.json');
+    const json = await resp.json();
+    this.setState({ ritten: json });
+  }
+
+  render() {
     return (
       <Container>
-        <Tabs defaultActiveKey="overzicht" id="kalender-overzicht">
-          <Tab eventKey="overzicht" title="Overzicht beschikbare ritten">
-            <RitOverview ritten={this.state.ritten}/>
-          </Tab>
-          <Tab eventKey="kalender" title="Kalender 2020">
-            Kalender 2020
-          </Tab>
-        </Tabs>
-        
+        <Paper square>
+          <Tabs
+            value={this.state.selectedTab}
+            indicatorColor="primary"
+            textColor="primary"
+            onChange={this.handleChange}
+            centered
+          >
+            <Tab label="Beschikbare ritten" />
+            <Tab label="Kalender 2020" />
+          </Tabs>
+        </Paper>
+
+        <TabPanel value={this.state.selectedTab} index={0}>
+          <RitOverview.default ritten={this.state.ritten} />
+        </TabPanel>
+        <TabPanel value={this.state.selectedTab} index={1}>
+          Kalender 2020
+        </TabPanel>
       </Container>
-      
     );
   }
+}
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
 }
--- src/RitOverview.tsx
+++ src/RitOverview.tsx
@@ -2,69 +2,76 @@ import React from 'react';
 import { Rit } from './Rit';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileDownload, faMountain, faRoute, faClock, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 export interface Props {
     ritten: Rit[];
 }
-export interface State {
 
+const useStyles = makeStyles({
+    card: {
+        width: 300,
+    },
+    media: {
+        height: 256,
+    },
+});
+
+export default function RitOverview(props: Props) {
+    const classes = useStyles();
+    const ritten = props.ritten.slice(0)
+        .sort((a: Rit, b: Rit) => a.ritnummer - b.ritnummer);
+
+    return (
+        <Grid container spacing={3}>
+            {ritten.map((r, index) => {
+                return <Grid item xs key={r.ritnummer}><Paper>{renderRit(r, classes)}</Paper></Grid>
+            })}
+        </Grid>
+    );
 }
 
-
-export class RitOverview extends React.Component<Props, State>{
-
-    constructor(props: Props) {
-        super(props);
-        this.state = {};
-    }
-
-    render(){
-        const rittenPerRow = 3;
-        const ritten = this.chunk(this.props.ritten
-            .sort((a: Rit, b: Rit) => a.ritnummer - b.ritnummer), rittenPerRow);
-        
-
-        return (
-            <Container>
-                {ritten.map((r,index) => {
-                    return <Row>{r.map(rit => this.renderRit(rit))}</Row>
-                })}
-            </Container>
-        );
-    }
-
-    chunk<T>(arr: T[], len: number): T[][] {
-        let chunks = [],
-            i = 0,
-            n = arr.length;
-
-        while (i < n) {
-            chunks.push(arr.slice(i, i += len));
-        }
-
-        return chunks;
-    }
-
-
-    renderRit(rit: Rit) {
-        const previewImage = rit.thumbnail ? (<Card.Img variant="top" src={rit.thumbnail} />) : "";
-        return (
-            <Col key={rit.ritnummer} md="4">
-                <Card style={{ width: '260' }} key={rit.ritnummer}>
-                    {previewImage}
-                    <Card.Body>
-                        <Card.Title>Rit {rit.ritnummer}: {rit.naam}</Card.Title>
-                        <Card.Text>
-                            <FontAwesomeIcon icon={faRoute} />&nbsp;{rit.afstand} km -&nbsp;
+function renderRit(rit: Rit, classes: Record<"card" | "media", string>) {
+    return (
+        <Card className={classes.card}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={rit.thumbnail ? rit.thumbnail : 'resources/unknown_location.png'}
+                    title="Minimap"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Rit {rit.ritnummer}: {rit.naam}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        <FontAwesomeIcon icon={faRoute} />&nbsp;{rit.afstand} km -&nbsp;
                             <FontAwesomeIcon icon={faMountain} />&nbsp;{rit.hoogtemeters} m -&nbsp;
                             <FontAwesomeIcon icon={faClock} />&nbsp;{rit.vertrek}<br />
-                            <Card.Link href={rit.gpx}><FontAwesomeIcon icon={faFileDownload} />&nbsp;Download GPX</Card.Link>
-                            <Card.Link href="#"><FontAwesomeIcon icon={faMapMarkedAlt} />&nbsp;Toon op kaart</Card.Link>
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </Col>
-        )
-    }
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                {rit.gpx ? 
+                <Button size="small" color="primary" href={rit.gpx}>
+                    <FontAwesomeIcon icon={faFileDownload} />&nbsp;Download GPX
+                    </Button> : 
+                    <Button size="small" color="primary" disabled>
+                        <FontAwesomeIcon icon={faFileDownload} />&nbsp;Download GPX
+                    </Button>}
+                <Button size="small" color="primary">
+                    <FontAwesomeIcon icon={faMapMarkedAlt} />&nbsp;Toon op kaart
+                    </Button>
+            </CardActions>
+        </Card>
+    )
 }
